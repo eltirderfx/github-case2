7c64c505bff0b64f4b0c869e1ce737041a8e0346
elwandytirtana <eltirderfx@gmail.com>
2020-04-18 18:14:52 +0700

    add main.js

--- index.php
+++ index.php
@@ -4,12 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fuhahahahaha</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Simple Notes</h1>
     <ul>
         <li>1</li>
         <li>2</li>
+        <li>3</li>
     </ul>
 </body>
 </html>
